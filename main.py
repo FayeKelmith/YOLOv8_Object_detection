19280a2915f7fcc6861036868eebdb881bbf033c
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-09-01 20:48:53 +0530

    rectangle and colors are good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO 
 import random
+import cv2
 
 # #NOTE: prefered training set
 # from datasets import load_dataset
@@ -9,30 +10,38 @@ import random
 #loading model
 model  = YOLO('yolov8n.pt')
 
-results = model.predict(source='dataset/room.jpg')[0]
+img = cv2.imread('dataset/room.jpg')
+img = cv2.resize(img,(0,0), fx =0.2,fy =0.2)
+results = model.predict(source=img)[0]
 
 #COLORING NAMES OF OBJECTS DETECTED
 names = list(set(results.boxes.cls.tolist()))
 colors = [tuple([random.randint(0,255) for _ in range(3)]) for _ in range(len(names))]
 cls_id_col = dict(zip(names,colors))
-print(cls_id_col)
+#print(cls_id_col)
+
 #Getting Info of objects detected
-# for result in results:
-#     box = result.boxes 
-#     cords = box.xywh[0].tolist()
-#     cords = [round(x) for x in cords]
-#     name = result.names[box.cls[0].item()]
-#     conf = round(box.conf[0].item(),2)
-#     print(f'Found {name} at {cords} with confidence: {conf}')
+for result in results:
+    box = result.boxes 
+    cords = box.xyxy[0].tolist()
+    cls = box.cls[0].item()
+    cords = [round(x) for x in cords]
+    name = result.names[box.cls[0].item()]
+    conf = round(box.conf[0].item(),2)
+    
+    #thresholding on the basis of confidence:
+    if(conf>=0.4):
+    #for rectangle on object
+        cv2.rectangle(img,(int(cords[0]),int(cords[1])),(int(cords[2]),int(cords[3])),color=cls_id_col[cls],thickness=2)
+    #text background color:
+        text_size,baseline = cv2.getTextSize(name,cv2.FONT_HERSHEY_COMPLEX,fontScale=0.4,thickness=1)
+        cv2.rectangle(img, (int(cords[0]), int(cords[1] - text_size[1] - baseline)), (int(cords[0] + text_size[0]), int(cords[1])), (50,50,50), -1)
+    
+    #to add text
+        cv2.putText(img,name,(int(cords[0]),int(cords[1])),cv2.FONT_HERSHEY_COMPLEX,fontScale=0.4,color=(255,255,255),thickness=1)
+    
+    
+cv2.imshow("Detected",img)
+cv2.waitKey(0)
 
-
-
-
-#BUG: Training is computationally expensive
-#results = model.train(data = load_dataset, epochs=2)
-#metrics = model.val()
-#print(metrics)
-
-
-#TODO: to set up evaluation
 
